c76488007e253ef456430158d1c82e1ae98a72dd
deleteme0 <email1calvin@gmail.com>
2024-02-28 23:18:26 +0530

    local all done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 
 
-export default function About() {
+export default function Login() {
     return (
         <div>
             This is login
---
+++ src/app/selection/displayroom.tsx
@@ -0,0 +1,23 @@
+
+function SingleSeat(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+function DoubleSeat(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default function DisplayRoom({rooms,activeRoom,setRooms}:{rooms:any,activeRoom:any,setRooms:any}) {
+    return(
+        <div>
+
+        </div>
+    )
+}
