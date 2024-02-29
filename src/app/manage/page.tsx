4e3b050a18fd32fef1b3aaabd7bf89288e5137f4
Aathavan-19 <aathavangnanam@gmail.com>
2024-02-29 14:11:40 +0530

    commit

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,18 +1,40 @@
 
 'use client';
 import { useState } from "react";
-
+import '../login/loginpage.css';
+import './room.css';
 function Room() {
 
     const [room,setRoom]=useState("");
+    const [ds,setds]=useState("");
+    const [ss,setss]=useState("");
+
+    const handleClick = () => {
+        console.log(room,ss,ds);
+        // username = "abv";
+        //history.push('/selectpage'); // Redirect to the selectpage route
+      }
     
+
+
     return (
-        
+        <div className="body">
         <div className="container">
+            <div>
+            <h1>Room Allocation</h1>
+            </div>
             <div className="room">
                 <label>Rooms:</label>
                 <input type="text" name="room" id="room" value={room} onChange={event=>{setRoom(event.target.value)}} />
             </div>
+            <div className="seat">
+                <label>Double Seat:</label>
+                <input type="text" name="Double" id="double" value={ds} onChange={event=>{setds(event.target.value)}} />
+                <label>Single Seat:</label>
+                <input type="text" name="Single" id="single" value={ss} onChange={event=>{setss(event.target.value)}} />
+            </div>
+            <button onClick={handleClick}>Submit</button>
+        </div>
         </div>
     );
 }
