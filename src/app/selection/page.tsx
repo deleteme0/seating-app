e48eaaee54aa263acb8646f971ff210372d41473
deleteme0 <email1calvin@gmail.com>
2024-03-04 15:04:31 +0530

    added local and online modes

--- src/app/selection/page.tsx
+++ src/app/selection/page.tsx
@@ -7,104 +7,115 @@ import ShowRooms from './showrooms';
 export default function Selection() {
 
     const [count,setCount] = useState(0)
-    //const [students,setStudents] = useState([
-    //    {
-    //        dept: "3CSE",
-    //        use:false,
-    //        rollnos: [1,2,3,4,5,6,7,8,9,10]
-    //    },
-    //    {
-    //        dept: "3ECE",
-    //        use:false,
-    //        rollnos: [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-    //    },
-    //    {
-    //        dept: "3ADS",
-    //        use:false,
-    //        rollnos: [2,3,4,5,6,7,8,9,10]
-    //    }
-    //])
-    //const [rooms,setRooms] = useState([
-    //    {
-    //        roomno: 102,
-    //        use: false,
-    //        single:10,
-    //        singletaken:4,
-    //        doublefull:10,
-    //        doublehalf:3,
-    //        doubletaken:6,
-    //        benches:[
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}]
-    //        ]
-    //    },
-    //    {
-    //        roomno: 105,
-    //        use: false,
-    //        single:10,
-    //        singletaken:4,
-    //        doublefull:10,
-    //        doublehalf:3,
-    //        doubletaken:6,
-    //        benches:[
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //        ]
-    //    },
-    //    {
-    //        roomno: 107,
-    //        use: false,
-    //        single:10,
-    //        singletaken:4,
-    //        doublefull:10,
-    //        doublehalf:3,
-    //        doubletaken:6,
-    //        benches:[
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //        ]
-    //    },
-    //    {
-    //        roomno: 108,
-    //        use: false,
-    //        single:10,
-    //        singletaken:4,
-    //        doublefull:10,
-    //        doublehalf:3,
-    //        doubletaken:6,
-    //        benches:[
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
-    //        ]
-    //    },
-    //    ]
-    //)
-
     const [students,setStudents] = useState([])
     const [rooms,setRooms] = useState([])
 
+    const local = true;
+    //const [students,setStudents] = useState([])
+    //const [rooms,setRooms] = useState([])
+
     useEffect(()=>{
+
+        if (local == true){
+            setStudents([
+                {
+                    dept: "3CSE",
+                    use:false,
+                    rollnos: [1,2,3,4,5,6,7,8,9,10]
+                },
+                {
+                    dept: "3ECE",
+                    use:false,
+                    rollnos: [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+                },
+                {
+                    dept: "3ADS",
+                    use:false,
+                    rollnos: [2,3,4,5,6,7,8,9,10]
+                }
+            ]
+            )
+            setRooms(
+                [
+                    {
+                        roomno: 102,
+                        use: false,
+                        single:10,
+                        singletaken:4,
+                        doublefull:10,
+                        doublehalf:3,
+                        doubletaken:6,
+                        benches:[
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}]
+                        ]
+                    },
+                    {
+                        roomno: 105,
+                        use: false,
+                        single:10,
+                        singletaken:4,
+                        doublefull:10,
+                        doublehalf:3,
+                        doubletaken:6,
+                        benches:[
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                        ]
+                    },
+                    {
+                        roomno: 107,
+                        use: false,
+                        single:10,
+                        singletaken:4,
+                        doublefull:10,
+                        doublehalf:3,
+                        doubletaken:6,
+                        benches:[
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                        ]
+                    },
+                    {
+                        roomno: 108,
+                        use: false,
+                        single:10,
+                        singletaken:4,
+                        doublefull:10,
+                        doublehalf:3,
+                        doubletaken:6,
+                        benches:[
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                            [{dept:"",rollno:"",selected:0},{dept:"",rollno:"",selected:0}],
+                        ]
+                    },
+                    ]
+
+            )
+        
+        }
+
         async function doGet(){
             let gstudents = await fetch(process.env.NEXT_PUBLIC_API+"/manage/student",{
                 method:"GET"
@@ -115,6 +126,8 @@ export default function Selection() {
         }
 
         async function doGetRooms() {
+
+            
             
             let grooms = await fetch(process.env.NEXT_PUBLIC_API+'/manage/hall',{
                 method:"GET"
